023faefb8f3820d3bec8ed6272d6af213df0045d
LahiruPethum <pethum013@gmail.com>
2021-12-16 10:55:52 +0530

    Update cal

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,6 +12,10 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        string op = "";
+        double result = 0;
+        bool isOParationPerform = false;
+
         public Form1()
         {
             InitializeComponent();
@@ -29,43 +33,84 @@ namespace WindowsFormsApp1
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            double num1 = double.Parse(textBox1.Text);
-            double num2 = double.Parse(textBox2.Text);
-            string op = textBox3.Text;
-
-            if (op == "+")
-            {
-                double ans = num1 + num2;
-                label5.Text = ans.ToString();
-            }
-            else if (op == "-")
-            {
-                double ans = num1 - num2;
-                label5.Text = ans.ToString();
-            }
-            else if (op == "*")
-            {
-                double ans = num1 * num2;
-                label5.Text = ans.ToString();
-            }
-            else if (op == "/")
-            {
-                double ans = num1 / num2;
-                label5.Text = ans.ToString();
-            }
-            else {
-                
-                label5.Text = "Invalid";
-            }
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "";
-            textBox2.Text = "";
-            textBox3.Text = "";
-            label5.Text = "";
+            
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            switch (op) {
+                case "+" :textBox1.Text = (result + double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "-":
+                    textBox1.Text = (result - double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "*":
+                    textBox1.Text = (result * double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "/":
+                    textBox1.Text = (result / double.Parse(textBox1.Text)).ToString();
+                    break;
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" || isOParationPerform) {
+                textBox1.Clear();
+            }
+            Button btn = (Button)sender;
+            if (btn.Text == ".")
+            {
+                if (!textBox1.Text.Contains(".")) {
+                    textBox1.Text = textBox1.Text + btn.Text;
+                }
+                
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + btn.Text;
+            }
+            isOParationPerform = false;
+        }
+
+        private void oparation(object sender, EventArgs e)
+        {
+            if (result != 0) {
+                button18.PerformClick();
+                Button btn = (Button)sender;
+                op = btn.Text;
+                result = double.Parse(textBox1.Text);
+                label1.Text = result + op;
+                isOParationPerform = true;
+            }
+            else
+            {
+                Button btn = (Button)sender;
+                op = btn.Text;
+                result = double.Parse(textBox1.Text);
+                label1.Text = result + op;
+                isOParationPerform = true;
+            }
+            
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            label1.Text = "";
         }
     }
+    
+
 }
